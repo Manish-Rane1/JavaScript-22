25237d438ccb70231df56b03bd7c9d2c0effe7e2
Manish Rane <manishrane182@gmail.com>
2022-10-04 16:22:09 +0530

    Project-22

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log("<-----Area of Triangle---->")
+let l = 5
+let b = 6
+let h = 7
+console.log("Area of triangle is ",l*b*h)
+
+console.log("<-----Extension of file---->")
+name = "file.png"
+extension = name.split('.').pop()
+console.log("File name is ",name)
+console.log("File extension is: ",extension);
+
+console.log("<-----Sum of two integers---->")
+let int1 = 30
+let int2 = 30
+console.log("First interger is: "+int1+" Second integer is: "+int2)
+int1==int2 ? console.log((int1+int2)*3) : console.log(int1+int2)
